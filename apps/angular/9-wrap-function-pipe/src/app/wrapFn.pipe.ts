99c2332729643f51b5c0f3a573fb071658e2af03
Abhishek Wadalkar <abhishek.wadalkar1@gmail.com>
2024-08-15 18:28:46 +0530

    feat(Answer 9): solution for angular wrap function pipe

---
+++ apps/angular/9-wrap-function-pipe/src/app/wrapFn.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from '@angular/core';
+@Pipe({
+  name: 'wrapFn',
+  standalone: true,
+})
+export class WrapFnPipe implements PipeTransform {
+  transform<T>(value: (...args: any[]) => T, ...args: any[]): T {
+    return value(...args);
+  }
+}
